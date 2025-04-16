d265b85d14716533836097543f91ad5ccb788359
nadra-m <nadrasmahamed@gmail.com>
2025-04-16 02:27:44 -0500

    Major changes made to project.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,54 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{
+  if (users.find(user => user.username === username)) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+  if ((users.find(user => user.username === username)) && (users.find(user => user.password === password))) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-//only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let {username, password} = req.body;
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({data: password}, "access", {expiresIn: '1 hour.'});
+    req.session.authorization {
+      accessToken,
+      username
+    }
+    return res.status(202).json({message: "You have successfully logged in!", accessToken});
+  } else {
+    return res.status(401).json({message: "These are not the correct credentials."});
+  }
+
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const bookReview = req.body.bookReview;
+  const username = req.session.authorization?.username;
+  const isbn = req.params.isbn;
+
+  if (!books[isbn]) {
+    return res.status(404).json({message: "This book cannot be found."})
+  }
+
+  if (!bookReview) {
+    return res.status(400).json({message: "There are no reviews available. Would you like to add one?"})
+  }
+
+  books[isbn].reviews[username] = bookReview;
+  return res.status(200).json({message: "Review has been successfully added!"})
+  
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,130 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({message: "Please provide a Username or Password."});
+  }
+
+  if (isValid(username)) {
+    const newUser = { username, password };
+    users.push(newUser);
+    return res.status(201).json({message: "New user has been created."})
+  }
+  return res.status(400).json({message: "This user already exists."})
 });
 
-// Get the book list available in the shop
+// HELPER FUNCTIONS //
+
+
+function getBooks(){ 
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(books)
+    }, 1000)
+  })
+}
+
+function getBooksByIsbn(isbn) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const book = books[isbn];
+
+      if (book) {
+        resolve(book);
+      } else {
+        reject("Book unavailable.")
+      }
+    }, 1000)
+  })
+}
+
+function getBooksByAuthor(author) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const filteredBooks = Object.values(books).filter(book =>
+        book.author.toLowerCase().includes(author)
+      );
+
+      if (filteredBooks.length > 0) {
+        resolve(filteredBooks);
+      } else {
+        reject("No books found by this Author.");
+      }
+    }, 1000)
+  })
+}
+
+function getBooksByTitle(title) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const filteredBooks = Object.values(books).filter(book =>
+        book.title.toLowerCase().includes(title)
+      );
+
+      if (filteredBooks.length > 0) {
+        resolve(filteredBooks);
+      } else {
+        reject("No books found with this Title.");
+      }
+    }, 1000)
+  })
+}
+
+
+// MAIN METHODS //
+
+
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  getBooks().then((allBooks) => {
+    res.status(200).send(allBooks)
+  })
 });
 
-// Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  getBooksByIsbn(isbn).then((book) => {
+    return res.status(200).json(book);
+  })
+  .catch((error) => {
+    return res.status(404).json({message: error});
+  })
  });
   
-// Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ const author = req.params.author.toLowerCase();
+ getBooksByAuthor(author).then((books) => {
+  res.status(200).send(books)
+ })
+ .catch((error) => {
+  res.status(404).json({message: error})
+ })
 });
 
-// Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+
+  getBooksByTitle(title).then((book) => {
+    res.status(200).json(book);
+  })
+  .catch((error) => {
+    res.status(404).json({message: error});
+  })
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book) {
+    return res.status(200).json(book.reviews);
+  } else {
+    return res.status(404).json({message: "This book cannot be found."})
+  }
 });
 
 module.exports.general = public_users;
